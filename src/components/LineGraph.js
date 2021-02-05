49f6288270d3887ed31aae4465115d89ffdc513d
unknown <quercia.matt@gmail.com>
2021-02-04 22:41:24 -0500

    Added Chart.JS and included a line graph to display quarterly earnings

---
+++ src/components/LineGraph.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Chart from "chart.js";
+let myLineChart;
+
+class LineGraph extends React.Component {
+    chartRef = React.createRef();
+    
+    componentDidMount() {
+        this.buildChart();
+    }
+
+    componentDidUpdate(){
+        this.buildChart();
+    }
+
+    buildChart = () => {
+        var myChartRef = this.chartRef.current.getContext("2d");
+        if (typeof myLineChart !== "undefined") myLineChart.destroy();
+
+        myLineChart = new Chart(myChartRef, {
+            type: "line",
+            data: {
+                //Bring in data
+                labels: ["4Q2019", "1Q2020", "2Q2020","3Q2020"],
+                datasets: [
+                    {
+                        label: "Quarterly Earnings",
+                        data: [this.props.data[0].actual.fmt, this.props.data[1].actual.fmt, this.props.data[2].actual.fmt, this.props.data[3].actual.fmt],
+                        backgroundColor: [
+                            'rgba(62, 97, 193, 1)'
+                        ]
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
+            </div>
+        )
+    }
+}
+
+export default LineGraph;
--- src/components/StockDetails.js
+++ src/components/StockDetails.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import LineGraph from './LineGraph'
 
-const StockDetails = (props) => {
-    if(props.stock !== null){
+import '../Styles/StockDetails.css'
+
+class StockDetails extends React.Component {
+    render(){
+        if(this.props.stock !== null && this.props.stock.earnings !== undefined){
+            return(
+                <div>
+                    <h2>{this.props.stock.symbol}</h2>
+                    <h3>{this.props.stock.price.longName}</h3>
+                    <p>{this.props.stock.price.regularMarketPrice.fmt}</p>
+                    <p>{this.props.stock.price.currency}</p>
+                    <p>{this.props.stock.price.exchangeName}</p>
+                    <div className="chart-size">
+                        <LineGraph
+                            data={this.props.stock.earnings.earningsChart.quarterly}
+                        />
+                    </div>
+                </div>
+            )
+        }
         return(
             <div>
-                <p>{props.stock.symbol}</p>
-                <p>{props.stock.price.regularMarketPrice.fmt}</p>
+                <p>No stock with that ticker was found or the ticker was not a stock.</p>
             </div>
         )
     }
-    return(
-        <div>
-            <p>No stock with that ticker was found.</p>
-        </div>
-    )
 };
 
 export default StockDetails;
